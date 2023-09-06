7d4babf8a5bf3555b396785e19b5b854d9a1ace8
Arseniy Gurin <arseniygurin93@gmail.com>
2023-09-06 17:50:49 +0300

    Practice-variables

---
+++ Variables/Variables.js
@@ -0,0 +1,19 @@
+/* -------- 1 --------- */
+let admin;
+let name;
+
+name = "John";
+
+admin = name;
+
+alert(admin);
+console.log(admin);
+
+/* -------- 2 --------- */
+
+let ourPlanet = 'Earth';
+
+let currentUserName = 'John'
+
+
+
